8e14998cd11b324b8034ec61532b4dc80eb821a2
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2024-09-11 08:59:50 +0530

    how render work

--- React/NAMASTE-REACT/app.js
+++ React/NAMASTE-REACT/app.js
@@ -1,3 +1,6 @@
 const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"hello World From React!");
+
+console.log(heading); // heading is an object
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(heading); // render method convert heading object in to tag and put inside dom
