445c459eadae4b7ee220b929a4b9e1b5740838c2
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2024-09-11 08:53:25 +0530

    adding css file

--- React/NAMASTE-REACT/app.js
+++ React/NAMASTE-REACT/app.js
@@ -1,3 +1,3 @@
-const heading = React.createElement("h1",{id:"heading"},"hello World From React!");
+const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"hello World From React!");
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
