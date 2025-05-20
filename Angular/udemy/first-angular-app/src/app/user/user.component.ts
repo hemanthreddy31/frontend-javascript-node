be30f91390ce8eae4e9dd68bc262ea0fa64e37f6
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2025-05-20 16:13:35 +0530

    configurable component

--- Angular/udemy/first-angular-app/src/app/user/user.component.ts
+++ Angular/udemy/first-angular-app/src/app/user/user.component.ts
@@ -9,12 +9,11 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required:true}) id!: string;
+ @Input({required:true}) id!: string;
  @Input({required:true}) avatar!: string;
  @Input({required:true}) name!: string;
 
-//  @Output() select  = new EventEmitter();
-select = output<string>();
+ @Output() select  = new EventEmitter<string>();
 
  get imagePath(){
   return 'assets/users/'+ this.avatar;
