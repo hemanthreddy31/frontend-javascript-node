77a6f7c29679562694035ba3ceee94ae1751b63c
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2025-05-20 15:55:22 +0530

    signal output function

--- Angular/udemy/first-angular-app/src/app/user/user.component.ts
+++ Angular/udemy/first-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output ,output} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -13,7 +13,8 @@ export class UserComponent {
  @Input({required:true}) avatar!: string;
  @Input({required:true}) name!: string;
 
- @Output() select  = new EventEmitter();
+//  @Output() select  = new EventEmitter();
+select = output<string>();
 
  get imagePath(){
   return 'assets/users/'+ this.avatar;
