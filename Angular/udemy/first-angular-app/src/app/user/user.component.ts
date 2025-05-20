327b6d282b649d8d76f6817757fbdcdb2a35465d
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2025-05-20 16:29:44 +0530

    Interface

--- Angular/udemy/first-angular-app/src/app/user/user.component.ts
+++ Angular/udemy/first-angular-app/src/app/user/user.component.ts
@@ -1,5 +1,17 @@
-import { Component, EventEmitter, Input, Output ,output} from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+
+// type User = {
+//   id:string;
+//   avatar: string;
+//   name: string;
+//  }
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -9,17 +21,14 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
- @Input({required: true}) user!: {
-  id:string;
-  avatar: string;
-  name: string;
- };
+  @Input({ required: true }) user!: User;
 
- @Output() select  = new EventEmitter<string>();
+  @Output() select = new EventEmitter<string>();
 
- get imagePath(){
-  return 'assets/users/'+ this.user.avatar;
- }
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+  
   onSelectUser() {
     this.select.emit(this.user.id);
   }
